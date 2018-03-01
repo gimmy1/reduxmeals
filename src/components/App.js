1cd472d87847c86b5b5a627defae228903eba777
gali7221 <gali7221@gmail.com>
2018-03-01 15:26:33 -0500

    remove vanilla redux and add high-level redux methods: connect and provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,52 @@
 import React, { Component } from 'react'
-import { addRecipe } from '../actions'
+import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
+
 
 class App extends Component {
-  state = {
-    calendar: null
-  }
-  componentDidMount () {
-    const { store } = this.props
-
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      },
-    }))
-
-    this.input.value = ''
-  }
+  // Our UI
   render() {
+    console.log("props", this.props);
     return (
       <div>
-        <input
-          type='text'
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-          Mondays Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+        Hello
       </div>
     )
   }
 }
-export default App
+
+// map our Redux State to Our Component props
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  //  Reformat the shape of our initialCalendarState
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  }
+}
+// Instead of exporting App let us now invoke connect and as a second paramter let us pass App
+// the funny syntax is Currying
+/*
+If you need to dispatch an action inside of a Component
+1. COnnect the components
+2. Then you'll be able to call dispatch
+*/
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 // import Redux libraries
 import { createStore } from 'redux';
 import reducer from './reducers';
+import { Provider} from 'react-redux'
+
 
 
 // in order to create store what you need to do is set invocation of createStore to a variable
@@ -27,8 +29,26 @@ const store = createStore(
 
 
 // Pass in store into our app component and now App is going to receive store as a prop
+
+// What if App component had a lot of subcomponents that each needed store?
+/*
+For example, dispatch something or needed access to the store
+Problem is we would have to pass the store down to all the sub components
+whenever they needed anything to do with store
+Our Hero - Provider
+**
+Provider = makes it possible to pass data through the component tree w/o having to pass the props
+down manually every level.**
+
+We pass store (Redux store) to Provider
+Whenever any components that app renders or app itself needs access to the store/dispatch action to store
+Becomes more easily
+Provider -
+*/
 ReactDOM.render(
-  <App store={store}/>,
+  <Provider store={store}>
+    <App store={store}/>
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
